00ececdd38223a94632dd089fcc84706ca79749d
jeremy <jeremy@trainerroad.com>
2017-10-17 08:50:58 -0700

    initial commit

---
+++ SoundSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SoundSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
